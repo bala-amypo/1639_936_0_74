d6d50afdab33bebfc2b271a1b1b0ce3b85b91a92
bala-amypo <bala@amypo.in>
2025-12-17 10:09:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,14 +2,16 @@ package com.example.demo.controller;
 import org.springframewok.beans.factory.annotation.Autowired;
 import org.springframewok.web.bind.annotation.RestController;
 import com.example.demo.service.StudentService;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.StudentEntity;
+import org.springframewok.web.bind.annotation.RequestBody;
+import org.springframewok.web.bind.annotation.PostMapping;
 
 
 @RestController
 public class StudentController{
     @Autowired StudentService ser;
 
-    @postMapping("/post")
+    @PostMapping("/post")
     public StudentEntity sendData(@RequestBody StudentEntity stu){
         return ser.postData(stu);
     }
