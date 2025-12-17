48086b6901a931de67bfc8a7a6ccc1a70c4d9763
bala-amypo <bala@amypo.in>
2025-12-17 10:14:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,10 +1,10 @@
 package com.example.demo.controller;
-import org.springframewok.beans.factory.annotation.Autowired;
-import org.springframewok.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.StudentService;
 import com.example.demo.entity.StudentEntity;
-import org.springframewok.web.bind.annotation.RequestBody;
-import org.springframewok.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
--- src/main/java/com/example/demo/service/Impl/SudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/SudentServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.service.StudentService;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.repository.StudentRepo;
 
 @Service
-@Override
 public class StudentServiceImpl implements StudentService{
     @Autowired StudentRepo repo;
     // save()
@@ -13,6 +13,7 @@ public class StudentServiceImpl implements StudentService{
     // findById()
     // deleteById()
     // exisById()
+    @Override
     public StudentEntity postData(StudentEntity stu){
         return student.save(stu);
     }
