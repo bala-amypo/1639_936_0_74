e88e5b6ecb741c49f14c61fda800512095057c36
bala-amypo <bala@amypo.in>
2025-12-18 09:09:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -5,35 +5,35 @@ import com.example.demo.service.ValidationService;
 import com.example.demo.entity.ValidationEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PutMapping;
 import java.util.List;
 
 
 @RestController
 public class ValidationController{
-    @Autowired StudentService ser;
+    @Autowired ValidationService ser;
 
     @PostMapping("/post")
-    public StudentEntity sendData(@RequestBody StudentEntity stu){
-        return ser.postData(stu);
+    public ValidationEntity sendData(@Valid @RequestBody ValidationEntity val{
+        return ser.postData(val);
     }
-    @GetMapping("/get")
-    public List<StudentEntity>getval(){
-        return ser.getAllData();
-    }
-    @DeleteMapping("/delete/{id}")
-    public String deleteData(@PathVariable int id){
-        return ser.DeleteData(id);
-    }
-    @GetMapping("/getid/{id}")
-    public StudentEntity getdata(@PathVariable int id){
-        return ser.getData(id);
-    }
-    @PutMapping("/put/{id}")
-    public StudentEntity putdata(@PathVariable int id,@RequestBody StudentEntity entity){
-        return ser.updateData(id,entity);
-    }
+    // @GetMapping("/get")
+    // public List<ValidationEntity>getval(){
+    //     return ser.getAllData();
+    // }
+    // @DeleteMapping("/delete/{id}")
+    // public String deleteData(@PathVariable long id){
+    //     return ser.DeleteData(id);
+    // }
+    // @GetMapping("/getid/{id}")
+    // public StudentEntity getdata(@PathVariable int id){
+    //     return ser.getData(id);
+    // }
+    // @PutMapping("/put/{id}")
+    // public StudentEntity putdata(@PathVariable int id,@RequestBody StudentEntity entity){
+    //     return ser.updateData(id,entity);
+    // }
 }
