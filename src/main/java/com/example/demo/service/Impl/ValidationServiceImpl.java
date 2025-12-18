ece1c9f8dbdebade6d6956b9fea84cd52d37fc82
bala-amypo <bala@amypo.in>
2025-12-18 09:44:03 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -1,42 +1,17 @@
-package com.example.demo.service.Impl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.example.demo.service.impl;
+import java.util.*;
 import com.example.demo.service.ValidationService;
-import com.example.demo.entity.ValidationEntity;
 import com.example.demo.repository.ValidationRepo;
-import java.util.List;
+import com.example.demo.entity.ValidationEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class ValidationServiceImpl implements ValidationService{
-    @Autowired ValidationRepo repo;
-    // save()
-    // findAll()
-    // findById()
-    // deleteById()
-    // exisById()
-    @Override
-    public ValidationEntity postData1(ValidationEntity val){
-        return repo.save(val);
-    }
-    // @Override
-    // public List<ValidationEntity>getAllData(){
-    //     return repo.findAll();
-    // }
-    // @Override
-    // public String DeleteData(Long id){
-    //     repo.deleteById(id);
-    //     return "Deleted Successfully";
-    // }
-    // @Override
-    // public ValidationEntity getData(Long id){
-    //     return repo.findById(id).orElse(null);
-    // }
-    // @Override
-    // public ValidationEntity updateData(int id,ValidationEntity entity){
-    //     if(repo.existsById(id)){
-    //         entity.setId(id);
-    //         return repo.save(entity);
-    //     }
-    //     return null;
-    // }
+     @Autowired ValidationRepo dent;
+     
+          @Override
+          public ValidationEntity post(ValidationEntity stu){
+               return dent.save(stu);
+          }
 }
