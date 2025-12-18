3f3e9a696a62d202ebb84b1245020a80984ca6cd
bala-amypo <bala@amypo.in>
2025-12-18 08:49:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/ValidationRepo.java
+++ src/main/java/com/example/demo/repository/ValidationRepo.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.StudentEntity;
+import com.example.demo.entity.ValidationEntity;
 
 @Repository
-public interface StudentRepo extends JpaRepository<StudentEntity,Integer>{
+public interface ValidationRepo extends JpaRepository<ValidationEntity,Long>{
         
 }
