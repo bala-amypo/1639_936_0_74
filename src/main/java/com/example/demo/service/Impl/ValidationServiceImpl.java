d593565b88a576eaf48b43674192fad4770e6406
bala-amypo <bala@amypo.in>
2025-12-19 04:40:05 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -15,5 +15,7 @@ public class ValidationServiceImpl implements ValidationService{
                return dent.save(stu);
           }
           @Override
-          public ValidationEntity getById(ValidationEntity )
+          public ValidationEntity getById(Long id){
+               return dent.findById(id).orElse(other:null);
+          }
 }
