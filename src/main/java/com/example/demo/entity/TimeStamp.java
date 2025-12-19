9c417b0d1e1c0abf843a5b47b8f508fee25ba76e
bala-amypo <bala@amypo.in>
2025-12-19 08:32:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStamp.java
+++ src/main/java/com/example/demo/entity/TimeStamp.java
@@ -42,16 +42,14 @@ public class TimeStamp{
     public void setPassword(String password){
         this.password=password;
     }
-    public Date getCreated(){
-        return created;
+    public LocalDateTime getCreateAt(){
+        return createAt;
     }
-    public void setCreated(Date created){
-        this.created=created;
-    }
-    public StudentEntity(){
+    public LocalDateTime
+    public TimeStamp(){
 
     } 
-    public StudentEntity(Integer id,String name,String password,String email,Date created){
+    public TimeStamp(Integer id,String name,String password,String email,Date created){
         this.id=id;
         this.name=name;
         this.password=password;
