135e9f0a0f06b7d087405062f06ca1b199ba4229
bala-amypo <bala@amypo.in>
2025-12-19 08:11:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TimeStampRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.TimeStamp;
+
+@Repository
+public interface TimeStampRepo extends JpaRepository<TimeStamp,Long>{
+        
+}
--- src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
@@ -1,14 +1,14 @@
 package com.example.demo.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.StudentService;
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.repository.StudentRepo;
+import com.example.demo.service.TimeStampService;
+import com.example.demo.entity.TimeStamp;
+import com.example.demo.repository.TimeStampRepo;
 import java.util.List;
 
 @Service
 public class TimeStampServiceImpl implements TimeStampService{
-    @Autowired StudentRepo repo;
+    @Autowired TimeStampRepo repo;
     // save()
     // findAll()
     // findById()
